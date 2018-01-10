56869c7616eaff9c7be2bfa3e9c905b2a11454d2
samu235 <samuel2351994@hotmail.es>
2018-01-10 23:58:29 +0100

    rev0
    programa en blanco

---
+++ desktop/src/com/juegosamu/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.juegosamu.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.juegosamu.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
